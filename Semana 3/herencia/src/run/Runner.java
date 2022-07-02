8f4e6b416fabd105313518e727919057807ede69
Jairo Armando <jairo.armando@gmail.com>
2022-07-01 19:19:21 -0500

    Semana 3 - Ejemplo Herencia clases abstractas(1)

--- Semana 3/herencia/src/run/Runner.java
+++ Semana 3/herencia/src/run/Runner.java
@@ -4,24 +4,26 @@ import logic.*;
 
 public class Runner {
     public static void main(String[] args) {
-        Terrestre gatico = new Terrestre("Mimi","Café con Negro",(byte)4,true,false);
-        Acuatico delfin = new Acuatico("Flipper","Rosado",(byte)2,false,"De agua salada o dulce");
-        Ave loro = new Ave("Roberto","Verde",(byte)2,true);
+        Terrestre gatico = new Terrestre("Mimi", "Café con Negro", (byte) 4, true, false);
+        Acuatico delfin = new Acuatico("Flipper", "Rosado", (byte) 2, false, "De agua salada o dulce");
+        Ave loro = new Ave("Roberto", "Verde", (byte) 2, true);
 
-        System.out.println( "Terrestre " + gatico.mamar());
-        System.out.println( "Acuatico " + delfin.mamar());
+        System.out.println("Terrestre " + gatico.mamar());
+        System.out.println("Acuatico " + delfin.mamar());
 
-        printAnimal( gatico );
-        printAnimal( delfin );
-        printAnimal( loro );
+        printAnimal(gatico);
+        printAnimal(delfin);
+        printAnimal(loro);
 
-        System.out.println( gatico );
+        System.out.println(gatico);
+
+
+    }
+
+    static void printAnimal(Animal animal) {
 
 
     }
 
-    static void printAnimal(Animal animal ){
-
-
-    }
+}
 
